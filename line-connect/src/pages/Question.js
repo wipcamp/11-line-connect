76cd101cac400233618e3d4d24cde5958b15b7b2
Joknoi <panupong.pj15@mail.kmutt.ac.th>
2019-01-21 17:05:48 +0700

    [UPDATE] set state q&a

--- line-connect/src/pages/Question.js
+++ line-connect/src/pages/Question.js
@@ -13,7 +13,7 @@ class Question extends Component {
         answer: 'กำลังโหลดคำตอบ',
         statusAns: 'disabled'
     }
-    componentWillMount = async() => {
+    componentWillMount = async () => {
         const url = new URLSearchParams(window.location.search)
         this.setState({ questionid: `${url.get('item')}` })
         const questionsformDB = await axios({
@@ -21,15 +21,25 @@ class Question extends Component {
             url: 'https://line-connect.freezer.in.th/api/question',
             data: {
                 JWT: Cookie.get('JWT'),
-                questionid:`${url.get('item')}`
+                questionid: `${url.get('item')}`
             }
         })
-        this.setState({ question: questionsformDB.data.question.content,
-            answer:questionsformDB.data.answer[0].ans_content })
+        if (questionsformDB.data.answer[0].ans_content) {
+            this.setState({
+                question: questionsformDB.data.question.content,
+                answer: questionsformDB.data.answer[0].ans_content
+            })
+        }else{
+            this.setState({
+                question: questionsformDB.data.question.content,
+                answer: ''
+            })
+        }
+
         // const getAnswer = await axios ({
         //     method: 'get',
         //     url: `https://registrant.service.freezer.in.th/api/answers/line/1`,
-      
+
         //     headers: {
         //         'Authorization': `Bearer ${ Cookie.get('JWT')}`,
         //         'Content-Type': 'application/json'
@@ -44,7 +54,7 @@ class Question extends Component {
                 <div className='col-12'>
                     <div className='row'>
                         <p>{this.state.questionid}. {this.state.question}</p><br></br>
-                        <textarea value={this.state.answer} disabled={this.state.statusAns} className='col-12' style={{height:'150px'}}></textarea>
+                        <textarea value={this.state.answer} disabled={this.state.statusAns} className='col-12' style={{ height: '150px' }}></textarea>
                         <div className='col-12 mt-3'>
                             <div className='row float-right'>
                                 <div className='inline-block mr-2'><button type="button" class="btn btn-warning">Warning</button></div>
