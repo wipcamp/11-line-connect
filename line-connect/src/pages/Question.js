ae79df65cb190d8d5dcd8e7c193333056e6ce7e7
Joknoi <panupong.pj15@mail.kmutt.ac.th>
2019-01-21 17:23:00 +0700

    [UPDATE] text box

--- line-connect/src/pages/Question.js
+++ line-connect/src/pages/Question.js
@@ -40,11 +40,17 @@ class Question extends Component {
         }
 
     }
-    handleAnswer() {
+    handleAnswerByButton() {
         this.setState({
             statusAns: ''
         })
     }
+    handleAnswer(e){
+        this.setState({
+            answer: e.target.value
+        })
+    }
+
     render() {
         return (
             <div className='container'>
@@ -52,10 +58,10 @@ class Question extends Component {
                 <div className='col-12'>
                     <div className='row'>
                         <p>{this.state.questionid}. {this.state.question}</p><br></br>
-                        <textarea value={this.state.answer} disabled={this.state.statusAns} className='col-12' style={{ height: '150px' }}></textarea>
+                        <textarea value={this.state.answer} disabled={this.state.statusAns} onChange={this.handleAnswer} className='col-12' style={{ height: '150px' }}></textarea>
                         <div className='col-12 mt-3'>
                             <div className='row float-right'>
-                                <div className='inline-block mr-2'><button type="button" class="btn btn-warning" onClick={this.handleAnswer} disabled={this.state.statusEdit}>แก้ไขคำตอบ</button></div>
+                                <div className='inline-block mr-2'><button type="button" class="btn btn-warning" onClick={this.handleAnswerByButton} disabled={this.state.statusEdit}>แก้ไขคำตอบ</button></div>
                                 <div className='inline-block'><button type="button" class="btn btn-info">Info</button></div>
                             </div>
                         </div>
