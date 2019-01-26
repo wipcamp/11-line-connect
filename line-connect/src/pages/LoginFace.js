f00a097cea3b82030793b51aed0804fc38283547
pepea <pepea.23@live.com>
2019-01-26 16:03:41 +0700

    [update] login

--- line-connect/src/pages/LoginFace.js
+++ line-connect/src/pages/LoginFace.js
@@ -3,7 +3,7 @@ import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props
 
 class LoginFace extends Component {
   responseFacebook () {
-    window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1638650000&redirect_uri=${window.env.PATH_FE}/login&state=asdasd&scope=openid%20profile`
+    window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1638650000&redirect_uri=${window.env.PATH_FE}&state=asdasd&scope=openid%20profile`
   }
   render () {
     return (
