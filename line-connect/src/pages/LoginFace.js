a809dcd1875415712b243b4f86f5deb8ea5fb81e
pepea <pepea.23@live.com>
2019-01-26 15:58:58 +0700

    [update] login path

--- line-connect/src/pages/LoginFace.js
+++ line-connect/src/pages/LoginFace.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
 
 class LoginFace extends Component {
-     responseFacebook(){
-    window.location.href=`https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1638650000&redirect_uri=${window.env.PATH_FE}&state=asdasd&scope=openid%20profile`
-    }
-    render() {
-        return (
-          <div className="App">
-            <div >
-              <FacebookLogin
-                appId="293604811359850"
-                version="3.2"
-                callback={this.responseFacebook}
-                render={renderProps => (
-                  <button size="large " block type="primary" onClick={renderProps.onClick}>Login!</button>
-                )}
-              />
-            </div>
-            </div>
-        );
-      }
+  responseFacebook () {
+    window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1638650000&redirect_uri=${window.env.PATH_FE}/login&state=asdasd&scope=openid%20profile`
+  }
+  render () {
+    return (
+      <div className='App'>
+        <div>
+          <FacebookLogin
+            appId='293604811359850'
+            version='3.2'
+            autoLoad={true}
+            fields='name,email,picture'
+            callback={this.responseFacebook}
+            render={renderProps => (
+              <button onClick={renderProps.onClick}>This is my custom FB button</button>
+            )}
+            ></FacebookLogin>
+        </div>
+      </div>
+    )
+  }
 }
 
-
-
-export default LoginFace;
+export default LoginFace
