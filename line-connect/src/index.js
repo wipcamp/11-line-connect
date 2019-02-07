0827b388b09dfaf2c6365982bf8f625c3732bcbb
J. PANUPONG <panupong.pj15@mail.kmutt.ac.th>
2019-02-08 01:17:05 +0700

    [ADD] add font sarabun

--- line-connect/src/index.js
+++ line-connect/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import styled from "styled-components";
 
+const Body = styled.div`
+  font-family: "Sarabun", sans-serif;
+`;
 
 const AppWithRoute = () => (
   <BrowserRouter>
-    <App />
+    <Body>
+      <App />
+    </Body>
   </BrowserRouter>
-)
+);
 
-ReactDOM.render(<AppWithRoute />, document.getElementById('root'));
+ReactDOM.render(<AppWithRoute />, document.getElementById("root"));
