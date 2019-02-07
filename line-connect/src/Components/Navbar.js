c1aa1eb2622d7e54dfbb2c7e1e92e417a97affe7
J. PANUPONG <panupong.pj15@mail.kmutt.ac.th>
2019-02-07 23:52:24 +0700

    [UPDATE] Navbar

--- line-connect/src/Components/Navbar.js
+++ line-connect/src/Components/Navbar.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import styled from 'styled-components'
+import React, { Component } from "react";
+import styled from "styled-components";
 
 const Bar = styled.div`
-    background-color: #304151;
-`
+  background-color: #304151;
+`;
 class Navbar extends Component {
-    render() {
-        return (
-            <Bar>
-                <ul class="nav justify-content-end" style={{height: '40px'}}>
-                    <li class="nav-item">
-                        <a class="nav-link" style={{color:'white',fontSize:'13px',height: '40px'}}>WIP ID : </a>
-                    </li>
-                </ul>
-            </Bar>
-        );
-    }
+  render() {
+    return (
+      <Bar>
+        <ul class="nav justify-content-end">
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              style={{ color: "white", fontSize: "13px", height: "40px" }}
+            >
+              WIP ID :{" "}
+            </a>
+          </li>
+        </ul>
+      </Bar>
+    );
+  }
 }
 
 export default Navbar;
