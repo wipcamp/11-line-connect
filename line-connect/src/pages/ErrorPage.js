7e3e2643b59b82709a9997e21aeb08e93031618d
J. PANUPONG <panupong.pj15@mail.kmutt.ac.th>
2019-02-11 20:16:05 +0700

    [ADD] Error Page

---
+++ line-connect/src/pages/ErrorPage.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { FaTimesCircle } from "react-icons/fa";
+require("dotenv").config();
+
+const Box = styled.div`
+  background-color: white;
+  box-shadow: 0px 0px 4px #d9d9d9;
+  border-radius: 4px;
+  position: relative;
+  z-index: 1;
+`;
+const Background = styled.img`
+  width: 100vw;
+  position: fixed;
+  bottom: 0;
+  z-index: 0;
+`;
+const Body = styled.div`
+  min-height: 100vh;
+  background-image: linear-gradient(#ffffff, #f8e9d6);
+`;
+const ButtonAnswer = styled.button`
+  border: 1px solid #304151;
+  background: #304151;
+  color: #fff;
+`;
+
+class ErrorNotRegister extends Component {
+  handleItim = () => {
+    window.location.href = `${window.env.PATH_ITIM}`;
+  };
+  render() {
+    return (
+      <Body>
+        <div className="container p-5">
+          <Box className="mt-5 p-5 text-center">
+            <p>
+              <FaTimesCircle className="mr-2" style={{ color: "#F5222D" }} />
+              ไม่สามารถเข้าสู่ระบบได้กรุณาลองใหม่อีกครั้งนะขอรับ
+            </p>
+            <ButtonAnswer type="button" className="btn pl-3 pr-3 mt-5">
+              ปิด
+            </ButtonAnswer>
+          </Box>
+        </div>
+        <Background src="/images/BG_Error.png" />
+      </Body>
+    );
+  }
+}
+
+export default ErrorNotRegister;
