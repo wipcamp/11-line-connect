ef6c8f9445e12cd3fa261d5973a892ff5860c763
pepea <pepea.23@live.com>
2019-02-17 19:05:49 +0700

    [UPDATE] test send msg

--- line-connect/src/pages/testLiff.js
+++ line-connect/src/pages/testLiff.js
@@ -16,6 +16,26 @@ class ErrorNotRegister extends Component {
   };
   handleClose = () => {
     const token = Liff.getAccessToken();
+     Liff.sendMessages([{
+      "type": "template",
+      "altText": "this is a confirm template",
+      "template": {
+          "type": "confirm",
+          "text": "Are you sure?",
+          "actions": [
+              {
+                "type": "message",
+                "label": "Yes",
+                "text": "yes"
+              },
+              {
+                "type": "message",
+                "label": "No",
+                "text": "no"
+              }
+          ]
+      }
+    }])
     this.setState({
       accesstoken: token
     });
