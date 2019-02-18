596f0b4df1d80445b836ae075c5a9439b7961e9e
J. PANUPONG <panupong.pj15@mail.kmutt.ac.th>
2019-02-18 18:38:23 +0700

    [UPDATE] test

--- line-connect/src/pages/CheckJWTPage.js
+++ line-connect/src/pages/CheckJWTPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import styled from "styled-components";
 import Cookies from "js-cookie";
+import { FaJournalWhills } from "react-icons/fa";
 
 const Liff = window.liff;
 
@@ -25,25 +26,27 @@ class ConnectPage extends Component {
       accessToken: accesstoken
     };
     await axios.post(`${window.env.PATH_AUTH}/auth/login`, line).then(JWT => {
-      Cookies.set("JWT", JWT.data.token);
-      Cookies.set("wip_id", JWT.data.wip_id);
-      if (JWT.data.wip_id) {
-        Cookies.set("JWT", JWT.data.token);
-        Cookies.set("wip_id", JWT.data.wip_id);
-        window.location.href = `${window.env.PATH_FE}/selectquestion`;
-      } else {
-        Cookies.set("accessToken", accesstoken);
-        Cookies.set("userId", userid);
-        window.location.href = `${window.env.PATH_FE}/`;
-      }
+      this.setState({
+        tokens: JWT.data.token,
+        wipid: JWT.data.wip_id
+      });
+      // if (JWT.data.wip_id) {
+      //   Cookies.set("JWT", JWT.data.token);
+      //   Cookies.set("wip_id", JWT.data.wip_id);
+      //   window.location.href = `${window.env.PATH_FE}/selectquestion`;
+      // } else {
+      //   Cookies.set("accessToken", accesstoken);
+      //   Cookies.set("userId", userid);
+      //   window.location.href = `${window.env.PATH_FE}/`;
+      // }
     });
   };
   render() {
     return (
       <div>
-        {this.state.tokens}
-        {this.state.wipid}
-        {this.state.token}
+        <p>jwt : {this.state.tokens}</p>
+        <p>wip id : {this.state.wipid}</p>
+        <p>{this.state.token}</p>
         {this.state.userid}
         <button onClick={this.handleConnect}>Click</button>
       </div>
